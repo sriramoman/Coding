8ecb803e48d59d3e4ff8c76649180b2ab5647ae1
Sriram Vellangallor Subramanian <sriramoman@gmail.com>
2016-10-29 20:10:09 -0700

    Arithmetic slices

--- src/CountNumbersUniqueDigits.java
+++ src/CountNumbersUniqueDigits.java
@@ -1,6 +1,15 @@
 /**
  * Created by sriram on 10/29/16.
  */
+/*
+Qn 357. Count Numbers with Unique Digits
+        Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10n.
+
+        Example:
+        Given n = 2, return 91. (The answer should be the total numbers in the range of 0 ≤ x < 100,
+        excluding [11,22,33,44,55,66,77,88,99])
+
+*/
 public class CountNumbersUniqueDigits {
     public int countNumbersWithUniqueDigits(int n) {
         if(n>10)
